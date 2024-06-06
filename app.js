ff342b339f04db064a431ed41b358ec67bff0ec9
n55567820 <n55567820@gmail.com>
2024-06-06 17:18:02 +0800

    新增user router: sign_up, sign_in, profile, updatePassword

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 const postRouter = require("./routes/posts");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
@@ -32,7 +33,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use('/', indexRouter);
+app.use('/', indexRouter);
+app.use("/users", usersRouter);
 app.use("/posts", postRouter);
 
 // 404 錯誤
---
+++ routes/users.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const appError = require("../service/appError");
+const jwt = require("jsonwebtoken");
+const handleErrorAsync = require("../service/handleErrorAsync");
+const validator = require("validator");
+const User = require("../models/usersModel");
+const { isAuth, generateSendJWT } = require("../service/auth");
+const router = express.Router();
+
+router.post("/sign_up", handleErrorAsync(async (req, res, next) => {
+    let { email, password, confirmPassword, name } = req.body;
+    // 內容不可為空
+    if (!email || !password || !confirmPassword || !name) {
+      return next(appError(400, "欄位未填寫正確！"));
+    }
+    // 密碼正確
+    if (password !== confirmPassword) {
+      return next(appError(400, "密碼不一致！"));
+    }
+    // 密碼 8 碼以上
+    if (!validator.isLength(password, { min: 8 })) {
+      return next(appError(400, "密碼字數低於 8 碼"));
+    }
+    // 是否為 Email
+    if (!validator.isEmail(email)) {
+      return next(appError(400, "Email 格式不正確"));
+    }
+
+    // 加密密碼
+    password = await bcrypt.hash(req.body.password, 12);
+    const newUser = await User.create({
+      email,
+      password,
+      name,
+    });
+
+    generateSendJWT(newUser, 201, res);
+  })
+);
+
+router.post("/sign_in", handleErrorAsync(async (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return next(appError(400, "帳號密碼不可為空"));
+    }
+    const user = await User.findOne({ email }).select("+password");
+    const auth = await bcrypt.compare(password, user.password);
+    if (!auth) {
+      return next(appError(400, "您的密碼不正確"));
+    }
+
+    generateSendJWT(user, 200, res);
+  })
+);
+
+router.get("/profile/", isAuth, handleErrorAsync(async (req, res, next) => {
+    res.status(200).json({
+      status: "success",
+      user: req.user,
+    });
+  })
+);
+
+router.post("/updatePassword", isAuth, handleErrorAsync(async (req, res, next) => {
+    const { password, confirmPassword } = req.body;
+
+    if (password !== confirmPassword) {
+      return next(appError(400, "密碼不一致！"));
+    }
+    const newPassword = await bcrypt.hash(password, 12);
+
+    const user = await User.findByIdAndUpdate(req.user.id, {
+      password: newPassword,
+    });
+
+    generateSendJWT(user, 200, res);
+  })
+);
+
+module.exports = router;
