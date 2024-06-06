3ddc265fe5217e98b285e84ff21392b5fcf56393
n55567820 <n55567820@gmail.com>
2024-06-06 15:25:35 +0800

    新增錯誤處理

--- app.js
+++ app.js
@@ -3,17 +3,27 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
-const dotenv = require("dotenv");
-dotenv.config();
 const indexRouter = require("./routes/index");
 const postRouter = require("./routes/posts");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = express();
+// 程式出現重大錯誤時
+process.on("uncaughtException", (err) => {
+  // 記錄錯誤下來，等到服務都處理完後，停掉該 process
+  console.error("Uncaughted Exception!");
+  console.error(err);
+  process.exit(1);
+});
 
-mongoose
-  .connect(process.env.DB_URL)
-  .then(() => console.log("資料庫連接成功"))
-  .catch(() => console.log("資料庫連接失敗"));
+dotenv.config();
+// 連線資料庫
+const DB = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(process.env.DB).then(() => console.log("資料庫連接成功"));
 
 app.use(cors());
 app.use(logger("dev"));
@@ -24,5 +34,61 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // app.use('/', indexRouter);
 app.use("/posts", postRouter);
+
+// 404 錯誤
+app.use(function (req, res, next) {
+  res.status(404).json({
+    status: "error",
+    message: "無此路由資訊",
+  });
+});
+
+// express 錯誤處理
+// 自己設定的 err 錯誤
+const resErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      message: err.message,
+    });
+  } else {
+    // log 紀錄
+    console.error("出現重大錯誤", err);
+    // 送出罐頭預設訊息
+    res.status(500).json({
+      status: "error",
+      message: "系統錯誤，請恰系統管理員",
+    });
+  }
+};
+// 開發環境錯誤
+const resErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+// 錯誤處理
+app.use(function (err, req, res, next) {
+  // ===== dev =====
+  err.statusCode = err.statusCode || 500;
+  if (process.env.NODE_ENV === "dev") {
+    return resErrorDev(err, res);
+  }
+  // ===== production =====
+  // mongoose 客製化
+  if (err.name === "ValidationError") {
+    err.message = "資料欄位未填寫正確，請重新輸入！";
+    err.isOperational = true;
+    return resErrorProd(err, res);
+  }
+  resErrorProd(err, res);
+});
+
+// 未捕捉到的 catch
+process.on("unhandledRejection", (err, promise) => {
+  console.error("未捕捉到的 rejection：", promise, "原因：", err);
+});
+
 
 module.exports = app;
